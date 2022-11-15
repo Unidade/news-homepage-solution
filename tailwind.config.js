b843049cd792ff47e44c30a1ca20851a24a71e75
Unidade <68622249+Unidade@users.noreply.github.com>
2022-11-15 19:19:18 -0300

    init repository

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      white: 'white',
+      'soft-orange': 'hsl(var(--soft-orange) /  <alpha-value>)',
+      'soft-red': 'hsl(var(--soft-red) /  <alpha-value>)',
+      'off-white': 'hsl(var(--off-white) /  <alpha-value>)',
+      'grayish-blue': 'hsl(var(--grayish-blue) /  <alpha-value>)',
+      'dark-grayish-blue': 'hsl(var(--dark-grayish-blue) /  <alpha-value>)',
+      'very-dark-blue': 'hsl(var(--very-dark-blue) /  <alpha-value>)',
+    },
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
